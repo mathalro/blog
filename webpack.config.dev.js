dfed9bb26f3bcba0f9ded1b7141f46f5656e7708
matheusp <matheus.ar11@hotmail.com>
2020-06-11 21:46:40 -0300

    first commit

---
+++ webpack.config.dev.js
@@ -0,0 +1,50 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map", // indicates that we can see the original code in the browser
+  entry: "./src/index",
+  output: {
+    // in development mode it will be in the memory, but it requires this folder
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  devServer: {
+    stats: "minimal",
+    overlay: true, // overlay any error that occurs in the browser
+    historyApiFallback: true, // all request will be sent to index.httml
+    // three last options is because an open issue in the last version of chrome
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false,
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+      "process.env.API_URL": JSON.stringify("http://localhost:5001"),
+    }),
+    new HtmlWebPackPlugin({
+      template: "src/index.html",
+      //favicon: "src/favicon.ico",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "eslint-loader"],
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
